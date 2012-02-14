db716b68dd4d528f19d1e6b30821ebefd4172a55
Marin Vila <rinmalavi@gmail.com>
2012-02-14 09:53:01 +0100

    Byte array test

--- pgjava-converters/src/main/java/hr/element/pgscala/converters/PGNullableByteArrayConverter.java
+++ pgjava-converters/src/main/java/hr/element/pgscala/converters/PGNullableByteArrayConverter.java
@@ -3,67 +3,67 @@ package hr.element.pgscala.converters;
 import org.joda.convert.*;
 
 public class PGNullableByteArrayConverter {
-  public static final String pgType = "bytea";
+	public static final String pgType = "bytea";
 
-  private static final char[] HEX_DIGITS =
-    "0123456789abcdef".toCharArray();
+	private static final char[] HEX_DIGITS = "0123456789abcdef".toCharArray();
 
-  private static final int[] HEX_INDEXES = new int['f' + 1]; static {
-    for (int i = 0; i < HEX_DIGITS.length; i ++) {
-      HEX_INDEXES[HEX_DIGITS[i]] = i;
-    }
-  }
+	private static final int[] HEX_INDEXES = new int['f' + 1];
+	static {
+		for (int i = 0; i < HEX_DIGITS.length; i++) {
+			HEX_INDEXES[HEX_DIGITS[i]] = i;
+		}
+	}
 
-  @ToString
-  public static String toPGString(final byte[] bA) {
-    return null == bA ? null : bA.toString();
-  }
+	@ToString
+	public static String toPGString(final byte[] bA) {
+		return null == bA ? null : quoteByteArray(bA);
 
-  @FromString
-  public static byte[] fromPGString(final String bA) {
-    return null == bA ? null : null;
-  }
+	}
+
+	@FromString
+	public static byte[] fromPGString(final String bA) {
+		return null == bA ? null : unquoteByteArray(bA);
+
+	}
+
+	public static String quoteByteArray(final byte[] bA) {
+		final int len = bA.length;
+		if (len == 0) {
+			return "";
+		}
+
+		final char[] buffer = new char[(len + 1) << 1];
+		{
+			buffer[0] = '\\';
+			buffer[1] = 'x';
+
+			int index = 2;
+			for (int i = 0; i < len; i++) {
+				final byte b = bA[i];
+				buffer[index++] = HEX_DIGITS[(b & 0xf0) >>> 4];
+				buffer[index++] = HEX_DIGITS[b & 0xf];
+			}
+		}
+
+		return new String(buffer);
+	}
+
+	public static byte[] unquoteByteArray(final String value) {
+		final int len = value.length();
+		if ((len == 0) || (len == 2)) {
+			return new byte[0];
+		}
+
+		final int newLen = ((len - 2) >> 1);
+		final byte[] buffer = new byte[newLen];
+
+		int index = 2;
+		for (int i = 0; i < newLen; i++) {
+			final char chH = value.charAt(index++);
+			final char chL = value.charAt(index++);
+			buffer[i] = (byte) ((HEX_INDEXES[chH] << 4) + HEX_INDEXES[chL]);
+		}
+
+		return buffer;
+	}
 }
-
-//  public static String quoteByteArray(final byte[] bA) {
-//    final int len = bA.length;
-//    if (len == 0) {
-//      return "''";
-//    }
-//
-//    final char[] buffer = new char[(len + 2) << 1];
-//    {
-//      buffer[0] = buffer[len - 1] = '\'';
-//      buffer[1] = '\\';
-//      buffer[2] = 'x';
-//
-//      int index = 3;
-//      for (int i = 0; i < len; i++) {
-//        final byte b = bA[i];
-//        buffer[index++] = HEX_DIGITS[b & 0xf0 >>> 4];
-//        buffer[index++] = HEX_DIGITS[b & 0xf];
-//      }
-//    }
-//
-//    return new String(buffer);
-//  }
-//
-//  public static byte[] unquoteByteArray(final String value) {
-//    final int len = value.length();
-//    if ((len == 2) || (len == 4)) {
-//      return new byte[0];
-//    }
-//
-//    final int newLen = (len >>> 1) - 2;
-//    final byte[] buffer = new byte[len];
-//
-//    int index = 3;
-//    for (int i = 0; i < newLen; i++) {
-//      final char chH = value.charAt(index ++);
-//      final char chL = value.charAt(index ++);
-//      buffer[i] = (byte) ( (REVERSE_HEX[chH] << 4) + REVERSE_HEX[chL]);
-//    }
-//
-//    return buffer;
-//  }
-//
---
+++ pgjava-converters/src/test/scala/hr/element/pgscala/converters/test/ByteArrayTest.scala
@@ -0,0 +1,97 @@
+package hr.element.pgscala.converters.test
+import org.scalatest.FunSuite
+import scala.collection.mutable.Stack
+import hr.element.pgscala.converters.PGNullableByteArrayConverter
+import org.scalatest.matchers.ShouldMatchers
+import scala.util.Random
+
+class ExampleSuite extends FunSuite
+  with ShouldMatchers {
+
+
+ test("String to byteArray test and back, 0 str length") {
+
+    val s = ""
+    val a = PGNullableByteArrayConverter.unquoteByteArray(s)
+    //println
+    //a.foreach(x => print("+" + x))
+    val s1 = PGNullableByteArrayConverter.quoteByteArray(a)
+    //println(" t>" + s + ", " + s1)
+
+    s should equal(s1)
+
+  }
+  test("String to byteArray test and back") {
+
+    val s = "\\xabcdef0123456789"
+    val a = PGNullableByteArrayConverter.unquoteByteArray(s)
+    //println
+    //a.foreach(x => print("+" + x))
+    val s1 = PGNullableByteArrayConverter.quoteByteArray(a)
+    //println(" t>" + s + ", " + s1)
+
+    s should equal(s1)
+
+  }
+
+  test("String to byteArray test and back 2") {
+
+    val s = "\\xaaccccddff1234"
+    val a = PGNullableByteArrayConverter.unquoteByteArray(s)
+    //println
+    //a.foreach(x => print("+" + x))
+    val s1 = PGNullableByteArrayConverter.quoteByteArray(a)
+    //println(" t>" + s + ", " + s1)
+
+    s should equal(s1)
+
+  }
+  test("String to byteArray test and back 3") {
+
+    val s = "\\xbbbb012345"
+    val a = PGNullableByteArrayConverter.unquoteByteArray(s)
+    //println
+    //a.foreach(x => print("+" + x))
+    val s1 = PGNullableByteArrayConverter.quoteByteArray(a)
+    //println(" t>" + s + ", " + s1)
+
+    s should equal(s1)
+
+  }
+
+
+    test("String to byteArray test and back 4") {
+
+    val s = "\\xbbbb012345"
+    val a = PGNullableByteArrayConverter.unquoteByteArray(s)
+    //println
+    //a.foreach(x => print("+" + x))
+    val s1 = PGNullableByteArrayConverter.quoteByteArray(a)
+    //println(" t>" + s + ", " + s1)
+
+    s should equal(s1)
+
+  }
+    test("Some random number procesing 10K."){
+      val randc = new Random(33)
+      val arr   = new Array[Byte](10000)
+      randc.nextBytes(arr)
+
+      val str1 = PGNullableByteArrayConverter.quoteByteArray(arr)
+      //println(str1)
+      val arr2 = PGNullableByteArrayConverter.unquoteByteArray(str1)
+
+      arr should equal (arr2)
+    }
+        test("Some random number procesing 1M."){
+      val randc = new Random(33)
+      val arr   = new Array[Byte](1000000)
+      randc.nextBytes(arr)
+
+      val str1 = PGNullableByteArrayConverter.quoteByteArray(arr)
+      //println(str1)
+      val arr2 = PGNullableByteArrayConverter.unquoteByteArray(str1)
+
+      arr should equal (arr2)
+    }
+}
