7233b552b7de3b1f84c682728ef700b529166cf8
Ana Marjanica <ana@element.hr>
2012-02-29 08:57:57 +0100

    new tests fro pgscala

--- pgjava-converters/src/test/scala/hr/element/pgscala/converters/test/ByteArrayTest.scala
+++ pgjava-converters/src/test/scala/hr/element/pgscala/converters/test/ByteArrayTest.scala
@@ -1,99 +1,88 @@
-//package hr.element.pgscala
-//package converters
-//package test
-//
-//import org.scalatest.FunSuite
-//import scala.collection.mutable.Stack
-//import org.scalatest.matchers.ShouldMatchers
-//import scala.util.Random
-//
-//class ExampleSuite extends FunSuite
-//  with ShouldMatchers {
-//
-//
-// test("String to byteArray test and back, 0 str length") {
-//
-//    val s = ""
-//    val a = PGNullableByteArrayConverter.stringToByteArray(s)
-//    //println
-//    //a.foreach(x => print("+" + x))
-//    val s1 = PGNullableByteArrayConverter.byteArrayToString(a)
-//    //println(" t>" + s + ", " + s1)
-//
-//    s should equal(s1)
-//
-//  }
-//  test("String to byteArray test and back") {
-//
-//    val s = "\\xabcdef0123456789"
-//    val a = PGNullableByteArrayConverter.stringToByteArray(s)
-//    //println
-//    //a.foreach(x => print("+" + x))
-//    val s1 = PGNullableByteArrayConverter.byteArrayToString(a)
-//    //println(" t>" + s + ", " + s1)
-//
-//    s should equal(s1)
-//
-//  }
-//
-//  test("String to byteArray test and back 2") {
-//
-//    val s = "\\xaaccccddff1234"
-//    val a = PGNullableByteArrayConverter.stringToByteArray(s)
-//    //println
-//    //a.foreach(x => print("+" + x))
-//    val s1 = PGNullableByteArrayConverter.byteArrayToString(a)
-//    //println(" t>" + s + ", " + s1)
-//
-//    s should equal(s1)
-//
-//  }
-//  test("String to byteArray test and back 3") {
-//
-//    val s = "\\xbbbb012345"
-//    val a = PGNullableByteArrayConverter.stringToByteArray(s)
-//    //println
-//    //a.foreach(x => print("+" + x))
-//    val s1 = PGNullableByteArrayConverter.byteArrayToString(a)
-//    //println(" t>" + s + ", " + s1)
-//
-//    s should equal(s1)
-//
-//  }
-//
-//
-//    test("String to byteArray test and back 4") {
-//
-//    val s = "\\xbbbb012345"
-//    val a = PGNullableByteArrayConverter.stringToByteArray(s)
-//    //println
-//    //a.foreach(x => print("+" + x))
-//    val s1 = PGNullableByteArrayConverter.byteArrayToString(a)
-//    //println(" t>" + s + ", " + s1)
-//
-//    s should equal(s1)
-//
-//  }
-//    test("Some random number procesing 10K."){
-//      val randc = new Random(33)
-//      val arr   = new Array[Byte](10000)
-//      randc.nextBytes(arr)
-//
-//      val str1 = PGNullableByteArrayConverter.byteArrayToString(arr)
-//      //println(str1)
-//      val arr2 = PGNullableByteArrayConverter.stringToByteArray(str1)
-//
-//      arr should equal (arr2)
-//    }
-//        test("Some random number procesing 1M."){
-//      val randc = new Random(33)
-//      val arr   = new Array[Byte](1000000)
-//      randc.nextBytes(arr)
-//
-//      val str1 = PGNullableByteArrayConverter.byteArrayToString(arr)
-//      //println(str1)
-//      val arr2 = PGNullableByteArrayConverter.stringToByteArray(str1)
-//
-//      arr should equal (arr2)
-//    }
-//}
+package hr.element.pgscala
+package converters
+package test
+
+import org.scalatest.FeatureSpec
+import org.scalatest.GivenWhenThen
+import org.scalatest.matchers.ShouldMatchers
+import scala.util.Random
+
+
+
+class ByteArrayTest extends FeatureSpec with GivenWhenThen with ShouldMatchers{
+  feature("About to test an String converter"){
+    info("I want to test if PGNullableByteArrayConverter works correctly, both in 2 way conversion")
+    info("I am going to perform tests for the String boundary cases")
+
+    scenario("String to Array[Byte] Nr. 1 test and back") {
+      info("test for select ''::bytea;")
+      given ("a starting value of zero String length")
+      val s = ""
+      when ("that value is transformed to Array[Byte] and back to String")
+      val bA  = PGNullableByteArrayConverter.stringToByteArray(s)
+      val res = PGNullableByteArrayConverter.byteArrayToString(bA)
+      then ("it should return an String value %s" format res)
+      res should equal(s)
+    }
+
+    scenario("String to Array[Byte] Nr. 2 test and back") {
+      info("test for select '\\xabcdef0123456789'::bytea;")
+      val s = "\\xabcdef0123456789"
+      given ("""a starting String value of  "%s"""" format s)
+      when ("that value is transformed to Array[Byte] and back to String")
+      val bA  = PGNullableByteArrayConverter.stringToByteArray(s)
+      val res = PGNullableByteArrayConverter.byteArrayToString(bA)
+      then ("it should return an String value %s" format res)
+      res should equal(s)
+    }
+
+    scenario("String to Array[Byte] Nr. 3 test and back") {
+      info("test for select '\\xaaccccddff1234'::bytea;")
+      val s = "\\xaaccccddff1234"
+      given ("""a starting String value of  "%s"""" format s)
+      when ("that value is transformed to Array[Byte] and back to String")
+      val bA  = PGNullableByteArrayConverter.stringToByteArray(s)
+      val res = PGNullableByteArrayConverter.byteArrayToString(bA)
+      then ("it should return an String value %s" format res)
+      res should equal(s)
+    }
+
+    scenario("String to Array[Byte] Nr. 4 test and back") {
+      info("test for select '\\xbbbb012345'::bytea;")
+      val s = "\\xbbbb012345"
+      given ("""a starting String value of  "%s"""" format s)
+      when ("that value is transformed to Array[Byte] and back to String")
+      val bA = PGNullableByteArrayConverter.stringToByteArray(s)
+      val res = PGNullableByteArrayConverter.byteArrayToString(bA)
+      then ("it should return an String value %s" format res)
+      res should equal(s)
+    }
+
+    scenario("Array[Byte] to String Nr. 1 test and back"){
+      info("test for some random number procesing 10K")
+      val randc = new Random(33)
+      val arr   = new Array[Byte](10000)
+      randc.nextBytes(arr)
+      given ("a starting Array[Byte] value of %s" format arr)
+      when ("that value is transformed to String and back")
+      val str1 = PGNullableByteArrayConverter.byteArrayToString(arr)
+      val arr2 = PGNullableByteArrayConverter.stringToByteArray(str1)
+      then ("it should return an Array[byte] value %s" format arr2)
+      arr should equal (arr2)
+    }
+
+    scenario("Array[Byte] to String Nr. 2 test and back"){
+      info("test for some random number procesing 1M")
+      val randc = new Random(33)
+      val arr   = new Array[Byte](1000000)
+      randc.nextBytes(arr)
+      given ("a starting Array[Byte] value of  %s" format arr)
+      when ("that value is transformed to String and back")
+      val str1 = PGNullableByteArrayConverter.byteArrayToString(arr)
+      val arr2 = PGNullableByteArrayConverter.stringToByteArray(str1)
+      then ("it should return an Array[byte] value %s" format arr2)
+      arr should equal (arr2)
+    }
+
+  }
+}
---
+++ pgjava-converters/src/test/scala/hr/element/pgscala/converters/test/ShortTest.scala
@@ -0,0 +1,73 @@
+package hr.element.pgscala
+package converters
+package test
+
+import org.scalatest.FeatureSpec
+import org.scalatest.GivenWhenThen
+import org.scalatest.matchers.ShouldMatchers
+import scala.util.Random
+
+
+
+class ShortTest extends FeatureSpec with GivenWhenThen with ShouldMatchers{
+  feature("About to test an Short converter"){
+    info("I want to test if PGNullableShortConverter works correctly, both in 2 way conversion")
+    info("I am going to perform tests for the Short boundary cases")
+
+    scenario("Short to String Nr. 1"){
+      given ("a starting Short value of Short.MaxValue")
+      val n = Short.MaxValue
+      when ("that value is converted to String")
+      val res = PGNullableShortConverter shortToString n
+      then ("""It should return a String value "%d"""" format n)
+      res should equal(n.toString)
+     }
+    scenario("Short to String Nr. 2"){
+      given ("a starting Short value of Short.MinValue")
+      val n = Short.MinValue
+      when ("that value is converted to String")
+      val res = PGNullableShortConverter shortToString n
+      then ("""It should return a String value "%d"""" format n)
+      res should equal(n.toString)
+    }
+    scenario("Short to String Nr. 3"){
+      given ("a starting Short value of 0")
+      val n: Short = 0
+      when ("that value is converted to String")
+      val res = PGNullableShortConverter shortToString n
+      then ("""It should return a String value "%d"""" format n)
+      res should equal(n.toString)
+    }
+
+    scenario("String to Short Nr. 1"){
+      info("test for select 32767::smallint;")
+      given ("a starting String value of Short.MaxValue")
+      val n = "32767"
+      when ("that value is converted to Short")
+      val res = PGNullableShortConverter stringToShort n
+      then ("It should return an Short value %s" format n)
+      res.toString should equal(n)
+    }
+    scenario("String to Short Nr. 2"){
+      info("test for select -32767::smallint;")
+      given ("a starting String value of Short.MinValue")
+      val n = "-32767"
+      when ("that value is converted to Short")
+      val res = PGNullableShortConverter stringToShort n
+      then ("It should return an Short value %s" format n)
+      res.toString should equal(n)
+    }
+    scenario("String to Short Nr. 3"){
+      info("test for select 0::smallint;")
+      given ("""a starting String value of "0"""")
+      val n = "0"
+      when ("that value is converted to Short")
+      val res = PGNullableShortConverter stringToShort n
+      then ("It should return an Short value %s" format n)
+      res.toString should equal(n)
+    }
+
+
+  }
+
+}
